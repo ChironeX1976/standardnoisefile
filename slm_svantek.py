e5e82e9d6e74c40383bef083f8300d00e2c080d5
m0564 <krisdebeleir@gmail.com>
2025-11-01 11:26:56 +0100

    bug fixed with audiopath

--- slm_svantek.py
+++ slm_svantek.py
@@ -30,7 +30,7 @@ def svantek_dataprep(decodeddata, fileproperties, audiofolder):
     df = markers_reorganize(df)
     # clean order columns
     lst_standard_spectrumcolumns = lst_standard_spectrumcolumn_names()
-    lst_always = [str_c_time, str_c_laeq1s, str_c_exclude, 'm2', 'm3', 'm4']
+    lst_always = [str_c_time, str_c_laeq1s, str_c_exclude, 'm2', 'm3', 'm4', str_c_soundpath]
     lst_always.extend(lst_standard_spectrumcolumns)
     df = df[lst_always]
     return df
