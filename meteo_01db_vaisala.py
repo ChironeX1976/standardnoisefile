26d0a2d209ec27a7a2293236725f1f706cdcb367
m0564 <krisdebeleir@gmail.com>
2026-01-11 14:01:48 +0100

    meteo vaisala from 01dB- added compass direction

--- meteo_01db_vaisala.py
+++ meteo_01db_vaisala.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 from io import BytesIO
 def meteo_01dB_vaisala_dataprep(decoded,fileproperties):
     # read dataset
@@ -22,8 +22,39 @@ def meteo_01dB_vaisala_dataprep(decoded,fileproperties):
     # Create 'exclude_meteo' as 1 if either invalid column is True, else 0
     df['exclude_meteo'] = (df['wind_invalid'] | df['rain_invalid']).astype(int)
 
+    # kompas-waarden volgens vlarem 2 bijlage 4.5.1 artikel 4§2
+    wind_dict = get_wind_compass_definitions(df['winddir_degrees'])
+
+    # np.select verwacht nog steeds twee lijsten, maar die halen we nu veilig uit de dict:
+    df['winddir_compass'] = np.select(
+        list(wind_dict.values()),  # De condities
+        list(wind_dict.keys()),  # De namen (N, NO, etc.)
+        default='VAR'
+    )
+
+
+
     # Filter and print the rows
-    filtered_df = df[df['exclude_meteo'] == 1]
-    print(filtered_df)
+    #filtered_df = df[df['exclude_meteo'] == 1]
+    #print(filtered_df)
     print(df)
     return df
+
+
+def get_wind_compass_definitions(series):
+    """
+    Geeft een dictionary terug met windstreek-definities.
+    Sleutels zijn de windstreken, waarden zijn de bijbehorende condities.
+    """
+    # We gebruiken een dictionary om de koppeling expliciet te maken
+    definitions = {
+        'N':  (series >= 337.5) | (series <= 22.5),
+        'NO': (series > 22.5)   & (series < 67.5),
+        'O':  (series >= 67.5)  & (series <= 112.5),
+        'ZO': (series > 112.5)  & (series < 157.5),
+        'Z':  (series >= 157.5) & (series <= 202.5),
+        'ZW': (series > 202.5)  & (series < 247.5),
+        'W':  (series >= 247.5) & (series <= 292.5),
+        'NW': (series > 292.5)  & (series < 337.5)
+    }
+    return definitions
